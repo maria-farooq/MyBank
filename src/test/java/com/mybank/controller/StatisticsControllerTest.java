131268031d4e652aea62b973d908156493c1eb6d
Maria Farooq <maria-farooq@live.com>
2018-05-12 13:54:18 +0500

    added test class for statistics endpoint testing

---
+++ src/test/java/com/mybank/controller/StatisticsControllerTest.java
@@ -0,0 +1,19 @@
+package com.mybank.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * This class will test various functionalities of <b>/statistics</b> endpoint
+ * 
+ * @author mariafarooq
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class StatisticsControllerTest {
+
+}
