1e7794dce494858b5a815290d3857a1de410e4fb
Maria Farooq <maria-farooq@live.com>
2018-05-12 12:24:31 +0500

    gerenerated mvn project with spring boot

---
+++ src/main/java/com/mybank/mybank/MybankApplication.java
@@ -0,0 +1,12 @@
+package com.mybank.mybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybankApplication.class, args);
+	}
+}
