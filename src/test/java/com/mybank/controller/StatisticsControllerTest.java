4538966f52f3d448f95e56dd42f4e1219eb58056
Maria Farooq <maria-farooq@live.com>
2018-05-12 14:44:35 +0500

    Added TEST CASE for getStatistics related to #2

--- src/test/java/com/mybank/controller/StatisticsControllerTest.java
+++ src/test/java/com/mybank/controller/StatisticsControllerTest.java
@@ -1,9 +1,19 @@
 package com.mybank.controller;
 
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 /**
  * This class will test various use cases of <b>/statistics</b> endpoint.
@@ -16,4 +26,35 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @AutoConfigureMockMvc
 public class StatisticsControllerTest {
 
+	@Autowired
+	private MockMvc mvc;
+	
+	/**
+	 * Endpoint: <b>/statistics</b>
+	 * </p> Http method: <b>GET</b>
+	 * </p> <b>Expected: </b>
+	 * </p> Http status 200
+	 * Returns:{"sum": ?,"avg": ?,"max": ?,"min": ?,"count": ?}
+	 * </p>
+	 * @throws Exception
+	 */
+	@Test
+    public void getStatistics() throws Exception {
+
+		final String sum=null;		// TODO: populate properly
+		final String avg=null;		// TODO: populate properly
+		final String max=null;		// TODO: populate properly
+		final String min=null;		// TODO: populate properly
+		final String count=null;	// TODO: populate properly
+
+		mvc.perform(MockMvcRequestBuilders.get("/statistics")
+				.accept(MediaType.APPLICATION_JSON))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.sum", is(sum)))
+                .andExpect(jsonPath("$.avg", is(avg)))
+				.andExpect(jsonPath("$.max", is(max)))
+				.andExpect(jsonPath("$.min", is(min)))
+				.andExpect(jsonPath("$.count", is(count)));			
+    }
 }
